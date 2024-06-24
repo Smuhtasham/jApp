f57c7dc3f4f49479e38ec6a7a4225edaec65f281
Syed <muhtasham00@gmail.com>
2024-06-25 00:55:39 +0500

    Colour change

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 export default function App() {
   return (
     <View>
       <Text>App</Text>
-      <Text>Syed Muhammad Muhtasham</Text>
+      <Text style={{color:"blue"}}>Syed Muhammad Muhtasham</Text>
+
     </View>
   )
 }
