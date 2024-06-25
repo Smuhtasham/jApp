209355295d884abb68db581cf6ad04bdd4823abf
Syed <muhtasham00@gmail.com>
2024-06-26 01:44:57 +0500

    Formate

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View>
       <Text>App</Text>
-      <Text style={{color:"blue"}}>Syed Muhammad Muhtasham</Text>
-
+      <Text style={{color: 'blue'}}>Syed Muhammad Muhtasham</Text>
     </View>
-  )
+  );
 }
