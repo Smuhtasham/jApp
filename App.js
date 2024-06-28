625642dcf20f202df5d0dd27b689dcb156fce196
Syed <muhtasham00@gmail.com>
2024-06-28 12:47:59 +0500

    View, Text , Buttons

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View>
-      <Text>App</Text>
-      <Text style={{color: 'blue'}}>Syed Muhammad Muhtasham</Text>
+      <Text style={{fontSize: 30}}>Hello Text Component</Text>
+      <Text style={{fontSize: 20}}>Syed Muhammad Muhtasham</Text>
+      <Button title="Press here"></Button>
+      <Button title="Press Me too"></Button>
     </View>
   );
 }
