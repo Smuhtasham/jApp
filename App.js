5183f1b2ff613d30cd5ed255f1c24cfb8d62912e
Syed <muhtasham00@gmail.com>
2024-06-28 13:15:32 +0500

    Component call & Button and onpress event

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
+import UserData from './Components/UserData';
 
 export default function App() {
+const fruit =(val)=>{
+  console.warn(val)
+}
+
   return (
     <View>
+      {/* Functional Component Call */}
+      <UserData />
       <Text style={{fontSize: 30}}>Hello Text Component</Text>
       <Text style={{fontSize: 20}}>Syed Muhammad Muhtasham</Text>
       <Button title="Press here"></Button>
-      <Button title="Press Me too"></Button>
+
+      <Button title="Press Me too" color={'blue'} onPress={()=>fruit("Hello")}/>
+      <Button title="Press Me too" color={'green'} onPress={fruit}/>
+
     </View>
   );
 }
+
---
+++ Components/UserData.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserData = () => {
+    return (
+      <View>
+        <Text style={{fontSize: 30}}>Name: Muhtasham</Text>
+        <Text style={{fontSize: 30}}>Age:23</Text>
+        <Text style={{fontSize: 30}}>EMail: muhtasham00@gmail.com</Text>
+      </View>
+    );
+  };
+
+  export default UserData;
